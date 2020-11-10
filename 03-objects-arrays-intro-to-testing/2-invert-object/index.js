a41023e90e351c74de6027f992b9729ef6cb228a
Alex Barenc <to-english@yandex.ru>
2020-11-10 19:32:19 +0300

    InvertObject task solved

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,12 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
-
+  const newObj = {};
+  if (!obj) {
+    return undefined
+  }
+  for (let [key, value] of Object.entries(obj)){
+    newObj[value] = key;
+  }
+  return newObj
 }
